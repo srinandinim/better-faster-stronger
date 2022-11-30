70771c5b8c9d15cdb874ed20d9566ab0d33cf018
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-11-30 11:45:57 -0500

    fixed negative sign, fixed caching, wip

--- game/model_optimalvaluefunction.py
+++ game/model_optimalvaluefunction.py
@@ -1,9 +1,11 @@
 import os, json, pickle 
+from itertools import islice 
 from graph import Graph 
 from copy import deepcopy 
 
 """
 Step 0: Retrieves the graph on which to run value iteration. 
+
 This functionality is copied from utils.py due to import dependencies/errors. 
 Note: do not remove GAME_GRAPH.json unless you can figure out how to retrieve it from graphs directory. 
 """
@@ -25,7 +27,6 @@ Step 1: Initialize Starting Distribution of State Values
 Terminal States: 
      agent_loc == pred_loc --> V(s) = -infty 
      agent_loc == prey_loc --> V(s) = 0 
-
 Non-Terminal States: 
     Heuristic Starting Distribution V(s) = bfs(agent_loc, prey_loc) * -1
     This initial seed heuristic enables value iteration to converge quickly
@@ -78,12 +79,95 @@ def init_state_values():
     return u0 
 
 """
-Step 2: Until convergence or a steady state, update non-terminal state values with Bellman Equations using Value Iteration. 
-
+Step 2: Until convergence or a steady state, update non-terminal state values with Bellman Equations using Value Iteration.
+- Initialize Hyperparameters: beta=0.9, eps=0.25, ksweeps=0, converged=False
+- Initialize v_t and v_t+1 so that we can do synchronous updates with value iteration
+- IF s is a terminal state: u_t+1(s) = u_t(s) 
+- IF s is a non terminal state: u_t+1(s) = max of all actions in action space (-1 + beta * sum over all states(probability of moving to s' * previous u_t(s')))
 """
+BETA_DISCOUNTFACTOR, EPSILON, ksweeps, converged = 0.90, 0.25, 0, False 
+u0, u1 = init_state_values(), dict() 
+
+# store the optimal pred locations cached
+optimal_pred_moves_cached = dict() 
+
+def sanity_check_value_updates(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+
+def optimal_pred_moves(agent_loc, pred_loc):
+    """
+    Returns all optimal locations pred can go to. 
+    """
+    pred_next_states = GAME_GRAPH.nbrs[pred_loc] + [pred_loc]
+
+    # stores the distances of all actions in the action space 
+    distances = dict() 
+
+    for pred_action in pred_next_states:
+
+        # compute -bfs(pred, agent) and cache that value for reuse
+        if (pred_action, agent_loc) not in optimal_pred_moves_cached:
+            distances[pred_action] = -1 * bfs(GAME_GRAPH, pred_action, agent_loc)
+            optimal_pred_moves_cached[(pred_action, agent_loc)] = distances[pred_action]
+
+        # retrieve cached value if it exists 
+        else: distances[pred_action] = optimal_pred_moves_cached[(pred_action, agent_loc)]
+    
+    # finds the shortest distance for the predator 
+
+    # gets all neighbors that result in shortest path 
+    shortest_distance = min(distances.values())
+    potential_moves = [] 
+    for key, value in distances.items():
+        if value == shortest_distance:
+            potential_moves.apend(key)
+    return potential_moves 
 
 
-u0 = init_state_values() 
+
+def transition_dynamics(agent_loc, prey_loc, pred_loc):
+    """
+    Returns next possible states for an action and their probabilities
+    """
+    new_states = dict() 
+
+    prey_next = GAME_GRAPH.nbrs[prey_loc] + [prey_loc]
+    pred_next = GAME_GRAPH.nbrs[pred_loc] + [pred_loc]
+
+
+# RUNS THE VALUE ITERATION ALGORITHM UNTIL CONVERGENCE
+while not converged: 
+
+    # iterate through all possible states
+    for agent_loc in range(1,51):
+        for prey_loc in range(1,51):
+            for pred_loc in range(1,51):
+
+                # determines the state we're currently at
+                state = (agent_loc, prey_loc, pred_loc)
+                
+                # retrieve old values for terminal states
+                if agent_loc == prey_loc or agent_loc == pred_loc: 
+                    u1[state] = u0[state]
+
+                # compute new values for non-terminal states
+                else: 
+                    agent_actions = GAME_GRAPH.nbrs[agent_loc] + [agent_loc]
+
+                    action_value = 0 
+                    for action in agent_actions:
+                        new_states = 
+
+
+
+
+
+                
+
+
+
+print(take(300, u0.values()))
 
 
 # Step 2: Until k sweeps of convergence, update the values
