580813a7a5f088cef6117d8a1a8512eeb9d85616
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-11-30 02:10:44 -0500

    pickles

---
+++ game/agents/agent1rl.py
@@ -0,0 +1,34 @@
+from .agent import Agent
+
+class Agent1RL(Agent):
+    def __init__(self, location):
+        # initialize agent location
+        super().__init__(location)
+
+    def move(self, graph, prey, predator):
+        """
+        updates location based on assignment specifications given
+        """
+        optimal_value_function = dict() 
+
+        action_value = dict() 
+        action_space = graph.get_node_neighbors(self.location) + [self.location]
+        
+        for action in action_space: 
+            action_value[action] = optimal_value_function[action]
+        
+        max_value = max(action_value.values())
+        
+        agent_action = self.location 
+        for action in action_space: 
+            if action_value[action] == max_value:
+                agent_action = action 
+        
+        self.location = agent_action 
+        return None, None 
+
+
+    def move_debug(self, graph, prey, predator):
+
+        return None, None
+
