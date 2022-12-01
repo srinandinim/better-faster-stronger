5c46988b31482207510035c861e9178eb917995f
Srinandini Marpaka <srinandinim@hotmail.com>
2022-12-01 14:15:37 -0500

    remove old code

--- game/agents/agent1rl.py
+++ game/agents/agent1rl.py
@@ -1,6 +1,7 @@
-import pickle 
-from .agent import Agent
+import pickle
 import random
+from .agent import Agent
+
 
 class Agent1RL(Agent):
     def __init__(self, location):
@@ -12,25 +13,11 @@ class Agent1RL(Agent):
         """
         updates location based on assignment specifications given
         """
-        action_value = dict() 
         action_space = graph.get_node_neighbors(self.location) + [self.location]
         best_utility = max([self.utility[(action, prey.location, predator.location)] for action in action_space])
         best_actions = [action for action in action_space if self.utility[(action, prey.location, predator.location)] == best_utility]
         self.location = random.choice(best_actions)
-        
-        # for action in action_space: 
-        #     action_value[action] = optimal_value_function[(action, prey.location, predator.location)]
-        
-        # #print(action_value)
-        
-        # max_value = max(action_value.values())
-        
-        # agent_action = self.location 
-        # for action in action_space: 
-        #     if action_value[action] == max_value:
-        #         agent_action = action 
-        
-        # self.location = agent_action 
+
         return None, None 
 
     def move_debug(self, graph, prey, predator):
