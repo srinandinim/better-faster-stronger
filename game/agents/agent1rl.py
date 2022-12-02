6dfccf779d172cfc4d88378f79dcc8d4d1ebc8f4
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-01 19:32:33 -0500

    video debug functionality

--- game/agents/agent1rl.py
+++ game/agents/agent1rl.py
@@ -21,5 +21,19 @@ class Agent1RL(Agent):
         return None, None 
 
     def move_debug(self, graph, prey, predator):
+        """
+        updates location based on assignment specifications given
+        """
+        action_space = graph.get_node_neighbors(self.location) + [self.location]
+        for action in action_space:
+            print(action, self.utility[(action, prey.location, predator.location)] )
+
+        best_utility = max([self.utility[(action, prey.location, predator.location)] for action in action_space])
+        best_actions = [action for action in action_space if self.utility[(action, prey.location, predator.location)] == best_utility]
+        self.location = random.choice(best_actions)
+
+        print(f"The action was to do {self.location}")
+
+
         return None, None
 
