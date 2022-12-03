12a984ad50a8598272e87c185d1895c4a630dc97
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-03 17:43:45 -0500

    fixed bug in autodiff

--- game/models/dnn.py
+++ game/models/dnn.py
@@ -6,13 +6,11 @@ class Neuron():
         self.w = [Node(random.uniform(-1,1)) for _ in range(n_inputs)]
         self.b = Node(random.uniform(-1,1))
 
-    def __call__(self, x, activated=True):
+    def __call__(self, x):
         unactivated = self.b
         for wi, xi in zip(self.w, x):
             unactivated += wi * xi
-
-        if activated: return unactivated.sigmoid()
-        else: return unactivated 
+        return unactivated.sigmoid()
         
     def parameters(self):
         return self.w + [self.b]
@@ -21,8 +19,8 @@ class Layer():
     def __init__(self, n_inputs, n_output):
         self.neurons = [Neuron(n_inputs) for _ in range(n_output)]
     
-    def __call__(self, x, activated=True):
-        outputs = [n(x, activated) for n in self.neurons]
+    def __call__(self, x):
+        outputs = [n(x) for n in self.neurons]
         return outputs[0] if len(outputs)==1 else outputs
     
     def parameters(self):
@@ -39,23 +37,15 @@ class DNN():
     
     def __call__(self, x):
         for i, layer in enumerate(self.layers): 
-
-            # the last layer is linear 
-            if i == len(self.layers)-1:
-                x = layer(x, activated=False)
-
-            # all other layers are non-linear
-            else: 
-                x = layer(x, activated=True)
-
+            x = layer(x)
         return x 
     
     def parameters(self):
         return [p for layer in self.layers for p in layer.parameters()]
 
-def compute_mse_loss():
+def compute_mse_loss_list(predictions, targets):
     loss = []
-    for y, y_hat in zip(ys, ypred):
+    for y, y_hat in zip(targets, predictions):
         y = Node(y)
         loss.append((y + (-y_hat) )**2)
     return loss 
@@ -66,67 +56,4 @@ def compute_total_loss(loss_list):
         total_loss += loss 
     return total_loss
 
-# arbitrary xi
-x = [2.0, 3.0, -1.0]
-
-# initialized DNN 
-dnn = DNN(3, [4,4,1])
-
-# initial output for arbitrary DNN
-print("initial output for arbitrary DNN")
-print(dnn(x))
-
-# how many network parameters to optimize
-print("how many network parameters to optimize")
-print(len(dnn.parameters()))
-
-# dataset for training
-xs = [
-    [2.0, 3.0, -1.0],
-    [3.0, -1.0, 0.5],
-    [0.5, 1.0, 1.0],
-    [1.0, 1.0, -1.0]
-]
-ys = [2.0, -2.0, -2.0, 2.0]
-
-# predict outputs for every item in training data
-print("predict outputs for every item in training data")
-ypred=[dnn(x) for x in xs]
-print(ypred)
-
-# compute error between true and predicted targets
-print("# compute error between true and predicted targets")
-loss_list = compute_mse_loss()
-print(loss_list)
-loss = compute_total_loss(loss_list)
-print(loss)
-
-# see the data and the gradients before
-print("see the data and the gradients before backprop")
-print(dnn.layers[0].neurons[0].w[0].data)
-print(dnn.layers[0].neurons[0].w[0].grad)
-
-# compute backprop on the layers
-loss.backward()
-
-# update the parameters of the network based on gradient descent
-for p in dnn.parameters():
-    p.data += -0.01 * p.grad
-
-# see the data and the gradients
-print("see the data and the gradients after backprop")
-print(dnn.layers[0].neurons[0].w[0].data)
-print(dnn.layers[0].neurons[0].w[0].grad)
-
-# predict outputs for every item in training data
-print("predict outputs for every item in training data")
-ypred=[dnn(x) for x in xs]
-print(ypred)
-
-# compute error between true and predicted targets
-print("# compute error between true and predicted targets")
-loss_list = compute_mse_loss()
-print(loss_list)
-loss = compute_total_loss(loss_list)
-print(loss)
 
