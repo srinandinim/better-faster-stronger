b4a6864d99285f5ee810b2e37fd51ec89b30a373
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-03 14:25:02 -0500

    hierarchy

---
+++ game/models/training_vcomplete.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+#  load X, Y targets into memory
+
+
+#  build a deep neural network
+
+
+#  for k epochs through datasets for minibatches of size m, run backprop
+
+
+#  save the model into a binary after it has been trained 
