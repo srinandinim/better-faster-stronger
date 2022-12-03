95228c3993f01cfbff3485cc32d995fb0b577f6b
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-03 14:25:02 -0500

    hierarchy

---
+++ game/models/utils_models.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+def get_training_data(data=np.loadtxt("supervised_dataset_vcomplete.csv", delimiter=",")):
+    """
+    reads through dataset and returns the Y, and X values of the data 
+    """
+    Y, X = data[:,0], data[:,1:]
+    return Y, X 
+
+if __name__ == "__main__":
+    Y, X = get_training_data()
+
+    print(f"y1 is {Y[1]}")
+    print(f"x1 is {X[1]}")
+    print(f"y2 is {Y[2]}")
+    print(f"x2 is {X[2]}")
