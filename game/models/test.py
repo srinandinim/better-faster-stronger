19614ac4d1edd989d956b37e6df620b24fce4335
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-04 14:10:09 -0500

    xor working

--- game/models/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-A = np.array([1.25488586, -2.74511414, -3.74511414, 1.25488586])
-
-# Use the reshape() method to convert A to a 2-dimensional array with 1 column and 4 rows
-B = A.reshape(4, 1)
-
-# Print the resulting array
-print(B)
