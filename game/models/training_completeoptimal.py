f8f659dc675c67aef47311d39664a02c18e64683
tejpshah <55666467+tejpshah@users.noreply.github.com>
2022-12-04 17:00:40 -0500

    working but error blowing up

--- game/models/training_completeoptimal.py
+++ game/models/training_completeoptimal.py
@@ -4,8 +4,21 @@ import numpy as np
 from utils_models import * 
 from fastneuralnet import *
 
-#  load X, Y targets into memory
+# load X, Y targets into memory
 Y,X = get_training_data()
+print(X.shape, Y.shape)
+#print(Y[0], X[0])
+X = np.array(X, dtype=np.float32)
+X = X.reshape((X.shape[0], 1, 150))
+print(X)
+
+Y = np.array(Y, dtype=np.float32)
+Y = Y.reshape((Y.shape[0], 1))
+print(Y)
+#print(Y[0], X[0])
+print(X.shape, Y.shape)
+
+
 print("Y, X are loaded into memory")
 
 # build out the neural network
@@ -21,8 +34,9 @@ dnn_v_complete.choose_error(mse, mse_prime)
 
 # train the model 
 start = time.time()
-dnn_v_complete.train(X[:20000], X[:20000], epochs=100, learning_rate=0.001)
-print(f"training took {time.time()-start} seconds")
+dnn_v_complete.train(X, Y, epochs=100, learning_rate=0.001)
+end = time.time()-start
+print(f"training took {end} seconds")
 
 # serialize the pickle 
 with open("dnn_v_complete.pkl", "wb") as file:
--- game/models/utils_models.py
+++ game/models/utils_models.py
@@ -4,19 +4,39 @@ def get_training_data(data=np.loadtxt("data/supervised_dataset_1hotvector_vcompl
     """
     reads through dataset and returns the Y, and X values of the data 
     """
-    Y, X = data[:,0], data[:,1:]
-    Y = Y.reshape((125000, 1, 1))
-    X = X.reshape((125000, 1, 150))
+    Y, X = data[:500,0], data[:500,1:]
+    Y[Y == -np.inf] = -50
     return Y, X
 
 if __name__ == "__main__":
 
-    """
+    
     Y, X = get_training_data()
     print(f"{Y.shape}, {X.shape}")
     print(f"y1 is {Y[1]}")
     print(f"x1 is {X[1]}")
     print(f"y2 is {Y[2]}")
     print(f"x2 is {X[2]}")
-    """
+    
+    X = np.array(
+    [
+        [ [0,0] ], 
+        [ [0,1] ], 
+        [ [1,0] ], 
+        [ [1,1] ]
+    ]
+    )
+
+    Y = np.array(
+        [
+            [[-9]], [[9]], [[8]], [[-27]]
+        ]
+    )
+
+    print(f"{Y.shape}, {X.shape}")
+    print(f"y1 is {Y[1]}")
+    print(f"x1 is {X[1]}")
+    print(f"y2 is {Y[2]}")
+    print(f"x2 is {X[2]}")
+
 
