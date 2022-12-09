8bcc3f8ac381bf4e12a3b56a3b9c8429a8fe7304
Srinandini Marpaka <srinandinim@hotmail.com>
2022-12-08 20:30:20 -0500

    game graph png

--- analysisquestions.py
+++ analysisquestions.py
@@ -17,29 +17,33 @@ def visualize_graph(graph_filename="GAME_GRAPH.json"):
 
     plt.show()
 
+    # figname = "graphs/" + graph_filename.split(".")[0] + ".png"
+    # plt.savefig(figname)
+
 
 if __name__ == "__main__":
-    # # find the state with the largest possible finite value of u*
-    # # load the pickle file with the optimal utilities
-    # OPTIMAL_UTILITIES_USTAR = pickle.load(open("game/pickles/OPTIMAL_U*.pickle", "rb"))
+    # find the state with the largest possible finite value of u*
+    # load the pickle file with the optimal utilities
+    OPTIMAL_UTILITIES_USTAR = pickle.load(open("game/pickles/OPTIMAL_U*.pickle", "rb"))
 
-    # min_ustar_value = 0 
-    # for utility in OPTIMAL_UTILITIES_USTAR.values():
-    #     if utility != -float("inf"):
-    #         min_ustar_value = min(utility, min_ustar_value)
+    min_ustar_value = 0 
+    for utility in OPTIMAL_UTILITIES_USTAR.values():
+        if utility != -float("inf"):
+            min_ustar_value = min(utility, min_ustar_value)
 
-    # largest_states = dict() 
-    # for state, utility in OPTIMAL_UTILITIES_USTAR.items():
-    #     if utility == min_ustar_value:
-    #         largest_states[state] = utility 
+    largest_states = dict() 
+    for state, utility in OPTIMAL_UTILITIES_USTAR.items():
+        if utility == min_ustar_value:
+            largest_states[state] = utility 
 
-    # # prints largest finite state and value
-    # print(largest_states)
+    # prints largest finite state and value
+    print(largest_states)
 
-    # # generates image of largest possible finite state and value
-    # g1 = Game() 
-    # g1.run_agent_1_rl_debug()
+    # generates image of largest possible finite state and value
+    g1 = Game() 
+    g1.run_agent_1_rl_debug()
 
+    # for graph generation
     visualize_graph()
 
 
