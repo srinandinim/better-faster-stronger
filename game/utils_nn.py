91416d42257c6d19ec5e4bcc61eaa3c959507f95
Srinandini Marpaka <srinandinim@hotmail.com>
2022-12-11 17:57:01 -0500

    code check of game module

--- game/utils_nn.py
+++ game/utils_nn.py
@@ -2,6 +2,7 @@ import pickle
 import random
 import numpy as np
 
+
 def vectorize_coordinate(coordinate, length=50):
     """
     takes a coordinate point and converts it to one hot vector. 
@@ -19,6 +20,7 @@ def vectorize_coordinate(coordinate, length=50):
             vector.append(0)
     return vector
 
+
 def vectorize_state(state):
     """
     takes a state and converts it to one hot vector matrix. 
@@ -32,6 +34,7 @@ def vectorize_state(state):
     """
     x, y, z = state
     return vectorize_coordinate(x) + vectorize_coordinate(y) + vectorize_coordinate(z)
+
 
 def create_supervised_training_data(utilities, graph_size=50):
     """
@@ -50,6 +53,7 @@ def create_supervised_training_data(utilities, graph_size=50):
     random.shuffle(yx)
     return yx
 
+
 def create_training_dataset_csv(utilities):
     """
     Dump the information from the input and output targets into a CSV file.
@@ -57,6 +61,7 @@ def create_training_dataset_csv(utilities):
     dataset = create_supervised_training_data(utilities=utilities)
     csv = np.asarray(dataset)
     np.savetxt("OPTIMAL_DATASET.csv", csv, delimiter=",", fmt='%.6f')
+
 
 def create_supervised_training_data_normalstates(utilities, graph_size=50):
     """
@@ -75,6 +80,7 @@ def create_supervised_training_data_normalstates(utilities, graph_size=50):
     random.shuffle(yx)
     return yx
 
+
 def create_training_dataset_normal_csv(utilities):
     """
     Dump the information from the input and output targets into a CSV file.
@@ -84,7 +90,9 @@ def create_training_dataset_normal_csv(utilities):
     np.savetxt("models/data/supervised_dataset_normalstates_vcomplete.csv",
                csv, delimiter=",", fmt='%.6f')
 
+
 if __name__ == "__main__":
-    OPTIMAL_COMPLETE_UTILITIES = pickle.load(open("game/pickles/OPTIMAL_U*.pickle", "rb"))
+    OPTIMAL_COMPLETE_UTILITIES = pickle.load(
+        open("game/pickles/OPTIMAL_U*.pickle", "rb"))
     create_training_dataset_csv(OPTIMAL_COMPLETE_UTILITIES)
     # create_training_dataset_normal_csv(OPTIMAL_COMPLETE_UTILITIES)
